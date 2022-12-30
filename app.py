3d35c4ce2e6baeedbd90dec5ef81afd8136317d3
Klop233 <3337913379@qq.com>
2022-12-30 17:32:02 +0800

    first commit

---
+++ app.py
@@ -0,0 +1,32 @@
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
+from flask import request
+import json
+
+app = Flask(__name__)
+CORS(app, supports_credentials=True)
+
+dict = {
+    "Klop233": "23333",
+    "dad": "114514"
+}
+
+
+@app.route('/get')
+def route_get():  # put application's code here
+    key = request.args.get("key")
+    if key not in dict.keys():
+        return jsonify({
+            "code": 404,
+            "data": ""
+        })
+
+    return jsonify({
+        "code": 200,
+        "data": dict[key]
+    })
+
+
+if __name__ == '__main__':
+    app.run()
